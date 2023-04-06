385c7f4ad930ee790eb0d860bf1f804404f24871
Scott Kelly <skellyaf@gmail.com>
2023-04-06 11:55:04 -0600

    sim

--- init_traj_files/init_simparams_cr3bp_bigHeo_to_mlo_3dv.m
+++ init_traj_files/init_simparams_cr3bp_bigHeo_to_mlo_3dv.m
@@ -83,13 +83,13 @@ simparams.add_tcm_improvement_threshold = sqrt(trace(simparams.R)) * 3;
 
 %% Trajectory parameter structure
 simparams.m = 7; % number of elements per trajectory segment (6 element state vector, 1 for time duration of segment)
-simparams.n = 30; % number of trajectory segments
+simparams.n = 25; % number of trajectory segments
 simparams.x0 = zeros(simparams.m, simparams.n); % empty storage for initial trajectory guess
 
 %% Trajectory options
 
 % Three nominal maneuvers
-simparams.maneuverSegments = [2, 13, simparams.n]; % the segments with an impulsive maneuver at their beginning
+simparams.maneuverSegments = [2, 10, simparams.n]; % the segments with an impulsive maneuver at their beginning
 simparams.P_constrained_nodes = simparams.maneuverSegments(2:end);
 simparams.max_num_TCMs = 6; % maximum number of TCMs per TCM optimization portion (between nominal maneuvers)
 
