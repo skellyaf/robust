2bc08f74270c57bbd79d574ea1c7a0de4596811a
Scott Kelly <skellyaf@gmail.com>
2023-11-03 09:18:57 -0600

    update sims for 100m Prmax Nrho rdvz from NRI arrival, c1

--- main_rdvz.m
+++ main_rdvz.m
@@ -32,7 +32,7 @@ clc;
 format longg;
 addpath(genpath('./'));
 
-savename = ['nrho_rdvz_robust_nriarrival_c2_corrNoms_fewersegs_10cmsnomDVerr_2tcms'];
+savename = ['nrho_rdvz_robust_c1_100mPrmax'];
 scenario = 'nrho rendezvous start at nri arrival';
 saveOutput = true; % bool for saving the output or not, true or false
 saveVideo = true;
@@ -95,8 +95,9 @@ outputPath = strcat('./sims/',dateString,'_',savename);
 % init_fn = './init_traj_files/init_sim_nrho_rdvz_2dv_tcmAtSegIntersection'; % Beginning just after perilune
 % init_fn = './init_traj_files/init_sim_nrho_rdvz_2dv_tcmAtSegIntersection_lead2'; % Beginning just after perilune
 % init_fn = './init_traj_files/init_sim_nrho_rdvz_2dv_tcmAtSegIntersection_lead3'; % Beginning at NRI w/100 km sep
-init_fn = './init_traj_files/init_sim_nrho_rdvz_2dv_tcmAtSegIntersection_lead4'; % case 1, beginning with previous 3 tcm opt solution
-init_fn = './init_traj_files/init_sim_nrho_rdvz_2dv_tcmAtSegIntersection_lead5'; % case 2, beginning with previous 3 tcm opt solution
+% init_fn = './init_traj_files/init_sim_nrho_rdvz_2dv_tcmAtSegIntersection_lead4'; % case 1, beginning with previous 3 tcm opt solution
+% init_fn = './init_traj_files/init_sim_nrho_rdvz_2dv_tcmAtSegIntersection_lead5'; % case 2, beginning with previous 3 tcm opt solution
+init_fn = './init_traj_files/init_sim_nrho_rdvz_2dv_tcmAtSegIntersection_lead6'; % case 1, beginning with 2 tcm opt, pr,max changed to 100m
 % init_fn = './init_traj_files/init_sim_nrho_rdvz_2dv_tcmSegI_startNRI'; % Beginning at NRI-ish
 
 
@@ -234,7 +235,7 @@ plotMultiSegTraj(x_opt, traj.x_t, traj.t_s, simparams, tcm_idx);
 % plotMultiSegTraj(x_opt, x_t, t_s, simparams);
 title('optim_solution_labels')
 solfig.CurrentAxes.Title.Visible="off";
-lg=legend('','','','','','','','','TCM','9:2 NRHO','','','Initial Chaser Position','Nominal Maneuver','','Rdvz No Later Than','Initial Target Position','Location','north')
+lg=legend('','','','','','','TCM','9:2 NRHO','','','Initial Chaser Position','Nominal Maneuver','','Rdvz No Later Than','Initial Target Position','Location','north');
 lg.Position(2)=.7;
 lg.Position(1)=.61;
 axis equal
@@ -248,11 +249,11 @@ solfig2.CurrentAxes.Title.Visible="off";
 axis equal
 view([90 0])
 % xlim([])
-% ylim([-.015 .015])
-% zlim([-.19 -.17])
+ylim([-.015 .015])
+zlim([-.19 -.17])
 
-ylim([.025 .045])
-zlim([-.16 -.12])
+% ylim([.025 .045])
+% zlim([-.16 -.12])
 
 
 
